692b23b94808401a38c16e5829d3e25a41c1de02
Liron <liron.hayman@gmail.com>
2016-08-17 17:34:17 +0300

    css adeed

--- messageboard/urls.py
+++ messageboard/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.IndexView.as_view(), name='index'),
+    url(r'^messageboard/submit-message$', views.submit_message, name='submit-message'),
 ]
--- messageboard/views.py
+++ messageboard/views.py
@@ -9,3 +9,25 @@ class IndexView(generic.ListView):
 
     def get_queryset(self):
         return Message.objects.order_by('-pub_date')[:5]
+
+
+# def submit_message(request):
+#     message=get_object_or404(message,pK=???)
+#     try:
+#         selected_choice = question.choice_set.get(pk=request.POST['choice'])
+#     except (KeyError, Choice.DoesNotExist):
+#         # Redisplay the question voting form.
+#         return render(request, 'polls/detail.html', {
+#             'question': question,
+#             'error_message': "You didn't select a choice.",
+#         })
+#     else:
+#         selected_choice.votes += 1
+#         selected_choice.save()
+#         # Always return an HttpResponseRedirect after successfully dealing
+#         # with POST data. This prevents data from being posted twice if a
+#         # user hits the Back button.
+#         return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+
+
+
