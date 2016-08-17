58239dc7cc00ed7f1289a09875e576b2fa1aef68
Liron <liron.hayman@gmail.com>
2016-08-17 15:51:45 +0300

    Full skeleton and DB created

--- messageboard/models.py
+++ messageboard/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    user_name = models.CharField(max_length=30, unique=True)
+
+    def __str__(self):
+        return self.user_name
+
+
+class Message(models.Model):
+    title = models.CharField(max_length=50)
+    text = models.CharField(max_length=250)
+    pub_date = models.DateTimeField('date published')
+    user_name = models.ForeignKey(User, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return self.title
+
--- messageboard/views.py
+++ messageboard/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import User, Message
 
-# Create your views here.
+
+class IndexView(generic.ListView):
+    template_name = 'messageboard/index.html'
+    context_object_name = 'last_messages'
+
+    def get_queryset(self):
+        return Message.objects.order_by('-pub_date')[:5]
